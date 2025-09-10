903e7846108d4c021f8caa5754c9a8c3c637ed07
Faith Chisepeya <chisepeyafaith@gmail.com>
2025-09-10 14:48:22 +0200

    updated script.js

--- src/script.js
+++ src/script.js
@@ -16,6 +16,8 @@ function refreshWeather(response){
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+    getForecast(response.data.city);
 }
 
 function formatDate(date){
@@ -52,7 +54,14 @@ function searchFunction(event){
     searchCity(searchInput.value);
 }
 
-function displayForecast(){
+function getForecast(city){
+    let apiKey = "3b3o0t0eda436e11fcf94a940f7accfe";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response){
+    console.log(response.data);
     let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecast = "";
 
@@ -75,4 +84,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchFunction);
 
 searchCity("Lilongwe");
-displayForecast();
+
